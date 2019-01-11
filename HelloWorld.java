85184ebecd7c10cc2de261bb8319aa5f94a802e0
BugsBunny <vishal.rathore100@gmail.com>
2019-01-11 15:34:38 +0530

    Java files modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 public static void main(String[] args)
 {
-String name = " Vishal Rathore";
+String name = " Vishal Kumar Rathore";
 System.out.println("Hello" + name);
 }
 }
